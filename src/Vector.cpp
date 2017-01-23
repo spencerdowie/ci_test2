6e2ec00c2361924d8167ac1377883e612770b418
spencer_dowie <spencer.dowie@uoit.net>
2017-01-23 16:10:06 -0500

    Working MathLib and MathLibTest (locally)

---
+++ src/Vector.cpp
@@ -0,0 +1,5 @@
+#include "Vector.h"
+
+Vector::Vector(float x, float y, float z)
+	: x(x), y(y), z(z)
+{ }
---
+++ src/Vector.h
@@ -0,0 +1,7 @@
+class Vector
+{
+	public:
+		Vector(float x = 0.0f, float y = 0.0f, float z = 0.0f);
+
+		float x, y, z;
+};
